0022b64fb16c874f9b8d2cbca6d196453a8be397
Roopesh <vkroopesh.chv@gmail.com>
2019-10-21 16:58:02 +0530

    form added

--- src/App.js
+++ src/App.js
@@ -1,26 +1,67 @@
 import React from 'react';
-import logo from './logo.svg';
+import ReactDOM from 'react-dom';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { firstname: '',
+                  lastname: '',
+                  age: '',
+                  designation: '',
+                  };
+  }
+  changeFirstname = (event) => {
+    this.setState({firstname: event.target.value});
+  }
+  changeLastname = (event) => {
+    this.setState({lastname: event.target.value});
+  }
+  changeAge = (event) => {
+    this.setState({age: event.target.value});
+  }
+  changeDesignation = (event) => {
+    this.setState({designation: event.target.value});
+  }
+  render() {
+    return (
+      <div>
+      <form>
+
+      <p>First name:</p>
+      <input
+        type='text'
+        onChange={this.changeFirstname}
+      />
+      <p>Last name:</p>
+      <input
+        type='text'
+        onChange={this.changeLastname}
+      />
+      <p>Age:</p>
+      <input
+        type='text'
+        onChange={this.changeAge}
+      />
+      <p>Designation:</p>
+      <input
+        type='text'
+        onChange={this.changeDesignation}
+      />
+
+      </form>
+
+      <div>
+        <h3>First Name : {this.state.firstname}</h3>
+        <h3>Last Name : {this.state.lastname}</h3>
+        <h3>Age : {this.state.age}</h3>
+        <h3>Designation : {this.state.designation}</h3>
+        </div>
+      </div>
+      
+
+    );
+  }
 }
 
 export default App;
